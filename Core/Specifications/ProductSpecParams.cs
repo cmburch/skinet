38a47fc2da96c7340e876ecf50fa42cb4f9702e7
Charles Burch <georgia_beast89@yahoo.com>
2020-07-04 02:47:34 -0400

    added search for product by Name

--- Core/Specifications/ProductSpecParams.cs
+++ Core/Specifications/ProductSpecParams.cs
@@ -11,9 +11,17 @@ namespace Core.Specifications
             get => _pageSize;
             set => _pageSize = (value > MaxPageSize) ? MaxPageSize : value;
         }
-
+        
         public int? BrandId { get; set; }
         public int? TypeId { get; set; }
         public string Sort { get; set; }
+
+        private string _search;
+
+        public string Search
+        {   
+            get => _search;
+            set => _search = value.ToLower();
+        }
     }
 }
